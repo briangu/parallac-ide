c6b7d14e740826e308cc37341fbf14c17a6e8a84
Brian Guarraci <brian@crickethealth.com>
2017-01-17 09:13:11 -0800

    update readme

--- default.js
+++ default.js
@@ -1,17 +1,15 @@
-{
-  let drawPoint = (x, y, color) => point(x + canvasMidX, y + canvasMidY, color)
-  let clearCanvas = () => clear()
+let drawPoint = (x, y, color) => point(x + canvasMidX, y + canvasMidY, color)
+let clearCanvas = () => clear()
 
-  writeFn(clearCanvas)
+writeFn(clearCanvas)
 
-  let colorIdx = 0
+let colorIdx = 0
 
-  for (let x = -50; x < 50; x += 10) {
-    for (let y = -50; y < 50; y += 10) {
-      let color = colorIdx % 2 === 0 ? "red" : "blue"
-      writeFn(drawPoint, x, y, color)
-    }
-
-    colorIdx++
+for (let x = -50; x < 50; x += 10) {
+  for (let y = -50; y < 50; y += 10) {
+    let color = colorIdx % 2 === 0 ? "red" : "blue"
+    writeFn(drawPoint, x, y, color)
   }
+
+  colorIdx++
 }
